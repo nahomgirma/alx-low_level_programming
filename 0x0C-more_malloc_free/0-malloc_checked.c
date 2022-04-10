8ea9ffa8318880bd7c3887eebd07167df9e2955d
nahomgirma <nahotigirma63@gmail.com>
2022-04-10 23:46:45 +0300

    message

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,16 @@
 #include <stdlib.h>
-#include <stdio.h>
+
 /**
- * malloc_checked - allocates memory using malloc
- * @b: size to be allocated
- * Return: pointer to allocated memory or exit with 98
- **/
+ * *malloc_checked - allocates memory using malloc and exit if failed
+ * @b: int
+ * Return: pointer to the array initialized or NULL
+ */
+
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+int *m = malloc(b);
+if (m == 0)
+	exit(98);
 
-	p = malloc(b);
-	if (p == NULL)
-		exit(98);
-	return (p);
+return (m);
 }
